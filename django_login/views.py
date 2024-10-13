8a0e1b5a0682c405e34912cb82ecb2c3e0d80aa1
keaguirre <ke.aguirre@duocuc.cl>
2024-10-13 03:41:48 -0300

    Fixed register_user view + new msg text

--- django_login/views.py
+++ django_login/views.py
@@ -15,11 +15,11 @@ def login_user(request):
         user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, 'You have successfully logged in')
+            messages.success(request, 'Has iniciado sesión de forma exitosa')
             return redirect('/', {'messages': messages, 'user': user.email})
         
         else:
-            error_msj= messages.error(request, 'Invalid email or password')
+            error_msj= messages.error(request, 'Email o contraseña incorrectos')
             return render(request, 'login.html', {'title': error_msj })
     else:
         return render(request, 'login.html', {'title': 'Login'})
@@ -30,17 +30,13 @@ def register_user(request):
         if form.is_valid():
             email = form.cleaned_data.get('username')  # Campo de entrada original de 'email'
             password = form.cleaned_data.get('password1')
-            
-            # Separar el correo
-            username = email.split('@')[0]  # Parte antes de '@' para el username
-
-            # Crear usuario con el username y email personalizados
+            username = email.split('@')[0]  # Parte antes de '@' para el username y guarda email desde el 'username' como correo que proveniente del formulario en el campo de email
             user = User.objects.create_user(username=username, email=email, password=password)
-            password = form.cleaned_data.get('password')
+            password = form.cleaned_data.get('password1')
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                messages.success(request, 'You have successfully registered')
+                messages.success(request, 'Registrado con éxito')
                 return redirect('/', messages)
             else:
                 error_msj = messages.error(request, user)
@@ -54,11 +50,11 @@ def register_user(request):
 
 def logout_user(request):
     if messages.success:
-        logout_msj = messages.success(request, 'You have successfully logged out')
+        logout_msj = messages.success(request, 'Has cerrado sesión de forma exitosa')
     elif messages.error:
-        logout_msj = messages.error(request, 'Error logging out')
+        logout_msj = messages.error(request, 'Error al cerrar sesión')
     else:
-        logout_msj = messages.error(request, 'Error logging out')
+        logout_msj = messages.error(request, 'Error al cerrar sesión')
     
     logout(request)
     return redirect('/', logout_msj)
